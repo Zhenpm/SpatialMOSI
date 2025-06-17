54a79d8005a7c5a66eb630886b9bb25e43b3b233
Zhenpm <150923384+Zhenpm@users.noreply.github.com>
2025-06-17 13:43:30 +0800

    Update __init__.py

--- SpatialMOSI/__init__.py
+++ SpatialMOSI/__init__.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
-"""
-# Author: Xiang Zhou
-# File Name: __init__.py
-# Description:
-"""
 
-__author__ = "Xiang Zhou"
-__email__ = "xzhou@amss.ac.cn"
 
 from .utils import generate_csl_graph, load_data, create_csp_dict, lsi, clr_normalization, generate_spatial_graph
 from .train import  train_SpatialMSI, train_SpatialMOSI
